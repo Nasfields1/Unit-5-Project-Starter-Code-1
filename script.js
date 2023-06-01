7bd75f6c1f8b324ba112af08b22fe49e77b19da9
Nasfields1 <135247658+Nasfields1@users.noreply.github.com>
2023-06-01 15:40:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let submitbutton-document.queryselector("-submitbutton'");
+let message=document querySelector(" message")
+let input = document. queryselector" comment")
+let Basketball=document.querySelector("-Basketball")
+let Captain America=document.queryselector(" Captain America")
+let result=document.querySelector(".result")
+
+submitbutton.onclick-function(){
+let input1= document. queryselector(" Basketball").value 
+  let input2= document. querySelector(" Captain America").value
+  
+if (input1==="basketball" && input2==="'Captain America" ){
+result. innerHTML= "because you picked " + inputs +" and " + inputs + " basketball is better"
+}
+  else if (input1=--"Basketball" && input2===
+"Iron Man"){
+result. innerHTML = "because you picked » + input1 + " and " + input + " Football is better"
+
+  }
+else if ( input1-="Football" && input2==="Captain America")
+result. innerHTML-"because you picked " + input1+ " and " + input2 + " you like Captain America more
+else if (input1---"Football" && input2==="Iron Man")
+result. innerHTML-"because you picked " + input1 + " and " + input2 + " you like Iron Man more
+7
+else{
+  }
+};
